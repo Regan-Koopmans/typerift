015bb3437803f92608810995d10cee6c9233109a
Regan Koopmans <regan.koopmans@takealot.com>
2019-07-06 14:24:57 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # typerift
+
 Create TypeScript interfaces from Scala case classes
+
+## Installation
+
+You can install typerift with the following command:
+
+```
+npm install -g typerift
+```
+
+## Usage
+
+You can use typerift in the following manner
+
+
+```
+typerift <File.scala>
+```
---
+++ main.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+if (process.argv.length < 3) {
+    console.log(chalk.red('Please provide a source file'))
+    return
+}
+
+let sourcePath = process.argv[2]
+
+const caseClassRegex = /(case)\s+(class)\s+(\w+)\s+\(\s*(\s*\w+\s*:\s*\w+\s*,?)*\s*\)/g
+const propertiesRegex = /\w+\s*:\s*\w+/g
+
+if (!fs.existsSync(sourcePath)) {
+    console.log(chalk.red('Could not find provided source file'))
+    return
+}
+
+let typeMap = {
+    Int: 'number',
+    String: 'string',
+    Double: 'number',
+    Float: 'number',
+    Boolean: 'boolean',
+    Char: 'char'
+}
+
+function getType(key) {
+    // TODO: Check for Optional and List
+
+    return typeMap[key] || 'any';
+}
+
+data = fs.readFileSync(sourcePath, 'utf8')
+results = Array.from(data.matchAll(caseClassRegex), x => x[0])
+
+results.forEach(caseClass => {
+
+    caseClassRegex.lastIndex = 0
+    let tokens = caseClassRegex.exec(caseClass)
+    let className = tokens[3]
+
+    console.log(`Generating TypeScript for class: ${chalk.yellow(className)}`)
+
+
+    let tsClass = `
+// Scallops v1.0.0 
+// Generated from ${sourcePath} on ${new Date()}
+// Do not modify this file directly!
+
+interface ${className} {\n`
+
+    let properties = caseClass.match(propertiesRegex)
+
+    properties.forEach(property => {
+        let pair = property.split(':')
+        pair = pair.map(x => x.trim())
+
+        let type = getType(pair[1])
+        tsClass += `    ${pair[0]}: ${type};  // ${pair[1]}\n`
+    })
+
+    tsClass += '}'
+
+    fs.writeFile(`${className}.ts`, tsClass, (err, file) => {
+
+        if (err) {
+            console.log(chalk.red(err))
+        }
+
+    })
+})
